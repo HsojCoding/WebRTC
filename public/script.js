a7086ff5df48775450a2115ef4f0ff3c441ea2be
Josh <99124562+HsojCoding@users.noreply.github.com>
2024-08-08 16:16:33 +0100

    Rewrite script.js
    Working prototype for WebRTC

--- public/script.js
+++ public/script.js
@@ -1,56 +1,111 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+var myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
 })
 const myVideo = document.createElement('video')
+let myId;
 myVideo.muted = true
 const peers = {}
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
-  addVideoStream(myVideo, stream)
 
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
-  })
+myPeer.on('open', async id => {
+    socket.emit('join-room', ROOM_ID, id)
+    console.log('Room ID: ', ROOM_ID)
+    console.log('Your Id: ', id)
+    myId = id;
+    try {
+        const localStream = await navigator.mediaDevices.getUserMedia({ 'video': true, 'audio': true });
+        addVideoStream(myVideo, localStream)
+        console.log('Local Video Loaded. ', localStream)
+    } catch(error) {
+        console.error('Failed to load local video. ', error)
+    }
+})
 
-  socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-  })
+myPeer.on('call', async call => {
+    try {
+        if (call.peer != myId) {
+            const localStream = await navigator.mediaDevices.getUserMedia({ 'video': true, 'audio': true });
+            call.answer(localStream);
+            console.log('Answered call with local stream.')
+            console.log('   From User: ', call.peer)
+            console.log('   Call: ', call)
+        }
+    } catch(error) {
+        console.error('Unable to answer call.')
+    }
+
+    try {
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+            console.log('Loaded Video Stream')
+        })
+        peers[call.peer] = {
+            callInfo: call,
+            videoElement: video
+        }
+    } catch(error) {
+        console.error('Failed to load peer call', error)
+    }
+
+})
+
+socket.on("user-connected", async userId => {
+    try {
+        if (userId != myId) {
+            console.log('User Connected: ' + userId)
+            const localStream = await navigator.mediaDevices.getUserMedia({ 'video': true, 'audio': true })
+            connectToNewPeer(userId, localStream)
+        }
+    } catch(error) {
+
+    }
 })
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+    if (peers[userId]) {
+        peers[userId].callInfo.close()
+        peers[userId].videoElement.remove()
+        console.log("Closed Connection: ", userId)
+        delete peers[userId]
+    };
 })
 
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
+function connectToNewPeer(userId, stream) {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        try {
+            addVideoStream(video, userVideoStream);
+            console.log('Video Stream Loaded:');
+            console.log('   User Id: ', userId);
+            console.log('   Stream Data: ', userVideoStream);
+        } catch(error) {
+            console.error('Video from: ', userId, ' Failed to load.', error)
+        }
+    });
+    call.on('close', () => {
+        if (video) video.remove();
+        console.log('Video Stream Dropped:');
+        console.log('   User Id: ', userId);
+    })
 
-function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
-  })
-  call.on('close', () => {
-    video.remove()
-  })
-
-  peers[userId] = call
+    peers[userId] = {
+        callInfo: call,
+        videoElement: video
+    }
 }
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
+    try {
+        video.srcObject = stream;
+        video.addEventListener('loadedmetadata', () => {
+            video.play();
+        });
+        videoGrid.append(video);
+    } catch(error) {
+        console.error('Failed to create video element. ', error);
+    }
 }
